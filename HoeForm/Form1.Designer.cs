e85879a691e750a3cd1877d67faf4f342ab2f6c9
Ichinose Shugo <ichinose@srmtlab.org>
2017-01-18 13:57:43 +0900

    リアルタイムカラー画像保存 WiiとKinect同時稼働

--- HoeForm/Form1.Designer.cs
+++ HoeForm/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rgbImage = new System.Windows.Forms.PictureBox();
+            this.RecordAll = new System.Windows.Forms.CheckBox();
+            this.RecordWii = new System.Windows.Forms.CheckBox();
+            this.RecordKinect = new System.Windows.Forms.CheckBox();
+            this.ax = new System.Windows.Forms.Label();
+            this.ay = new System.Windows.Forms.Label();
+            this.az = new System.Windows.Forms.Label();
+            this.limit = new System.Windows.Forms.Label();
+            this.jointNum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.rgbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rgbImage
+            // 
+            this.rgbImage.Location = new System.Drawing.Point(2, 91);
+            this.rgbImage.Name = "rgbImage";
+            this.rgbImage.Size = new System.Drawing.Size(640, 480);
+            this.rgbImage.TabIndex = 0;
+            this.rgbImage.TabStop = false;
+            // 
+            // RecordAll
+            // 
+            this.RecordAll.AutoSize = true;
+            this.RecordAll.Location = new System.Drawing.Point(2, 2);
+            this.RecordAll.Name = "RecordAll";
+            this.RecordAll.Size = new System.Drawing.Size(74, 16);
+            this.RecordAll.TabIndex = 1;
+            this.RecordAll.Text = "RecordAll";
+            this.RecordAll.UseVisualStyleBackColor = true;
+            this.RecordAll.CheckedChanged += new System.EventHandler(this.RecordAll_CheckedChanged);
+            // 
+            // RecordWii
+            // 
+            this.RecordWii.AutoSize = true;
+            this.RecordWii.Location = new System.Drawing.Point(2, 25);
+            this.RecordWii.Name = "RecordWii";
+            this.RecordWii.Size = new System.Drawing.Size(75, 16);
+            this.RecordWii.TabIndex = 2;
+            this.RecordWii.Text = "RecordWii";
+            this.RecordWii.UseVisualStyleBackColor = true;
+            // 
+            // RecordKinect
+            // 
+            this.RecordKinect.AutoSize = true;
+            this.RecordKinect.Location = new System.Drawing.Point(89, 25);
+            this.RecordKinect.Name = "RecordKinect";
+            this.RecordKinect.Size = new System.Drawing.Size(92, 16);
+            this.RecordKinect.TabIndex = 3;
+            this.RecordKinect.Text = "RecordKinect";
+            this.RecordKinect.UseVisualStyleBackColor = true;
+            // 
+            // ax
+            // 
+            this.ax.AutoSize = true;
+            this.ax.Location = new System.Drawing.Point(0, 44);
+            this.ax.Name = "ax";
+            this.ax.Size = new System.Drawing.Size(25, 12);
+            this.ax.TabIndex = 4;
+            this.ax.Text = "x軸:";
+            // 
+            // ay
+            // 
+            this.ay.AutoSize = true;
+            this.ay.Location = new System.Drawing.Point(0, 60);
+            this.ay.Name = "ay";
+            this.ay.Size = new System.Drawing.Size(25, 12);
+            this.ay.TabIndex = 5;
+            this.ay.Text = "y軸:";
+            // 
+            // az
+            // 
+            this.az.AutoSize = true;
+            this.az.Location = new System.Drawing.Point(0, 76);
+            this.az.Name = "az";
+            this.az.Size = new System.Drawing.Size(24, 12);
+            this.az.TabIndex = 6;
+            this.az.Text = "z軸:";
+            // 
+            // limit
+            // 
+            this.limit.AutoSize = true;
+            this.limit.Location = new System.Drawing.Point(87, 44);
+            this.limit.Name = "limit";
+            this.limit.Size = new System.Drawing.Size(56, 12);
+            this.limit.TabIndex = 7;
+            this.limit.Text = "ListCount:";
+            // 
+            // jointNum
+            // 
+            this.jointNum.AutoSize = true;
+            this.jointNum.Location = new System.Drawing.Point(87, 59);
+            this.jointNum.Name = "jointNum";
+            this.jointNum.Size = new System.Drawing.Size(56, 12);
+            this.jointNum.TabIndex = 8;
+            this.jointNum.Text = "JointNum:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 575);
+            this.Controls.Add(this.jointNum);
+            this.Controls.Add(this.limit);
+            this.Controls.Add(this.az);
+            this.Controls.Add(this.ay);
+            this.Controls.Add(this.ax);
+            this.Controls.Add(this.RecordKinect);
+            this.Controls.Add(this.RecordWii);
+            this.Controls.Add(this.RecordAll);
+            this.Controls.Add(this.rgbImage);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.rgbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox rgbImage;
+        private System.Windows.Forms.CheckBox RecordAll;
+        private System.Windows.Forms.CheckBox RecordWii;
+        private System.Windows.Forms.CheckBox RecordKinect;
+        private System.Windows.Forms.Label ax;
+        private System.Windows.Forms.Label ay;
+        private System.Windows.Forms.Label az;
+        private System.Windows.Forms.Label limit;
+        private System.Windows.Forms.Label jointNum;
     }
 }
 
--- HoeForm/Form1.cs
+++ HoeForm/Form1.cs
@@ -7,15 +7,229 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Kinect;
+using WiimoteLib;
+using System.IO;
 
 namespace HoeForm
 {
     public partial class Form1 : Form
     {
+        private KinectSensor kinect = null;
+        Wiimote wm = new Wiimote();
+
+        static public DateTime dt = DateTime.Now;//日付
+        static string pathDoc = Environment.GetFolderPath(Environment.SpecialFolder.Personal);//マイドキュメントのパス
+        static string path = null;
+        StreamWriter swk;//kinectファイル書き込み用
+        StreamWriter sww;//wiiファイル書き込み用
+
+        int frameKinect = 0;//フレームカウント
+        int frameWii = 0;
+        private List<Bitmap> bmp = new List<Bitmap>();
+        private List<byte[]> color = new List<byte[]>();
+        private List<int> frameList = new List<int>();
+        byte[] colorData = new byte[640 * 480 * 4];
+
         public Form1()
         {
-
+            Control.CheckForIllegalCrossThreadCalls = false;
             InitializeComponent();
+            InitFile();
+            InitKinect();
+            InitWii();
+        }
+
+        private void InitFile()
+        {
+            //ファイル書き込み用のdirectoryを用意
+            string date = dt.Year + digits(dt.Month) + digits(dt.Day) + digits(dt.Hour) + digits(dt.Minute);
+            path = pathDoc + "/HoeData/" + date;
+            Directory.CreateDirectory(path);
+            Directory.CreateDirectory(path + "/bmp");
+            //ファイルを用意
+            //frameKinect,frameWii,x,y,z
+            swk = new StreamWriter(path + "/Kinect.csv", false);
+            //frameWii,frameKinect,x,y,z
+            sww = new StreamWriter(path + "/Wii.csv", false);
+        }
+
+        private void InitKinect()
+        {
+            try
+            {
+                if (KinectSensor.KinectSensors.Count == 0)
+                    throw new Exception("Kinectが接続されていません");
+
+                // Kinectセンサーの取得
+                kinect = KinectSensor.KinectSensors[0];
+
+                //スケルトン平滑化パラメータ
+                TransformSmoothParameters parameters = new TransformSmoothParameters();
+                parameters.Smoothing = 0.2f;
+                parameters.Correction = 0.8f;
+                parameters.Prediction = 0.0f;
+                parameters.JitterRadius = 0.5f;
+                parameters.MaxDeviationRadius = 0.5f;
+
+                //color,skeletonの有効化(引数は解像度とフレームレート)
+                kinect.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                kinect.SkeletonStream.Enable(parameters);
+
+                //カラーフレームのイベントハンドラの登録
+                kinect.ColorFrameReady += ColorImageReady;
+
+                //スケルトンフレームのイベントハンドラの登録
+                kinect.SkeletonFrameReady += SkeletonFrameReady;
+
+                //Kinectセンサーからのストリーム取得を開始、以降、Kinectランタイムからフレーム毎に登録したColorFrameReadyメソッドが呼び出される
+                kinect.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+        }
+
+        private void InitWii()
+        {
+            wm.WiimoteChanged += wm_WiimoteChanged;//イベント登録
+            wm.SetReportType(InputReport.ButtonsAccel, true);//イベント呼び出しタイプの設定
+            wm.Connect();//Wiiリモコン接続
+        }
+
+        private void wm_WiimoteChanged(object sender, WiimoteChangedEventArgs e)
+        {
+            WiimoteState ws = e.WiimoteState;
+            //ファイル書き込み
+            if (RecordWii.Checked)
+                sww.WriteLine(frameWii + "," + frameKinect + ","+ ws.AccelState.Values.X + "," + ws.AccelState.Values.Y + "," + ws.AccelState.Values.Z);
+            //Formを書き換え
+            if (RecordWii.Checked)
+            {
+                ax.Text = "x軸:" + ws.AccelState.Values.X;
+                ay.Text = "y軸:" + ws.AccelState.Values.Y;
+                az.Text = "z軸:" + ws.AccelState.Values.Z;
+            }
+            frameWii++;
+        }
+
+        private void SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            //スケルトンフレームのデータをskeletonFrame変数に保持
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                //スケルトンフレームがnullでないならそのまま
+                if (skeletonFrame != null)
+                {
+                    //人数分のスケルトンデータ変数を配列で作る
+                    Skeleton[] skeletonData = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    //配列に各人物のスケルトンフレームのデータをコピー
+                    skeletonFrame.CopySkeletonDataTo(skeletonData);
+
+                    //スケルトンを認識した人数の数だけ繰り返す
+                    foreach (var skeleton in skeletonData)
+                    {
+                        //その人物のスケルトンがTracked状態なら続ける
+                        if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            jointNum.Text = "JointNum:" + skeleton.Joints.Count;
+                            //各ジョイントの数だけ繰り返す
+                            foreach (Joint joint in skeleton.Joints)
+                            {
+                                if (RecordKinect.Checked)
+                                    swk.WriteLine(frameKinect + "," + frameWii + ","
+                                       + joint.Position.X + ","
+                                       + joint.Position.Y + ","
+                                       + joint.Position.Z);
+                                /*
+                                    swc.WriteLine(frameKinect + "," + frameWii + ","
+                                       + (skeleton.Position.X - skeleton.Joints[JointType.HipCenter].Position.X) + ","
+                                       + (skeleton.Position.Y - skeleton.Joints[JointType.HipCenter].Position.Y) + ","
+                                       + (skeleton.Position.Z - skeleton.Joints[JointType.HipCenter].Position.Z));
+                                */
+                            }
+                            if (RecordKinect.Checked)
+                                swk.WriteLine(frameKinect + "," + frameWii + ","
+                                    + skeleton.Position.X + ","
+                                    + skeleton.Position.Y + ","
+                                    + skeleton.Position.Z);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ColorImageReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            //ColorImageFrameデータを貰う
+            using (ColorImageFrame imageFrame = e.OpenColorImageFrame())
+            {
+                if (imageFrame != null)
+                { 
+                    imageFrame.CopyPixelDataTo(colorData);
+                    unsafe
+                    {
+                        fixed (byte* ptr = &colorData[0])
+                        {
+                            Bitmap bmpImage = new Bitmap(
+                                        imageFrame.Width,
+                                        imageFrame.Height,
+                                        imageFrame.BytesPerPixel * imageFrame.Width,
+                                        System.Drawing.Imaging.PixelFormat.Format32bppRgb,
+                                        (IntPtr)ptr);
+                            rgbImage.Image = bmpImage;
+                            if (RecordKinect.Checked&&frameKinect%3==0)
+                            {
+                                //bmp.Add(bmpImage);
+                                //color.Add((byte[])colorData.Clone());
+                                frameList.Add(frameKinect);
+                                bmpImage.Save(path + "/bmp/" + frameKinect + ".bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                            }
+                            limit.Text = "ListCount:" + bmp.Count;
+                        }
+                    }
+                }
+                frameKinect++;
+            }
+        }
+
+        private void RecordAll_CheckedChanged(object sender, EventArgs e)
+        {
+            RecordKinect.CheckState = RecordAll.CheckState;
+            RecordWii.CheckState = RecordAll.CheckState;
+        }
+
+        public String digits(int date)
+        {
+            if (date / 10 == 0) return "0" + date;
+            else return date.ToString();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            int cnt = 0;
+            if(bmp.Count>=1)
+            foreach (var b in bmp)
+            {
+                b.Save(path + "/bmp/" + frameList[cnt] + ".bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                cnt++;
+            }
+            
+            /*
+            foreach (var c in color)
+            {
+                ImageConverter imgconv = new ImageConverter();
+                Image img = (Image)imgconv.ConvertFrom(c);
+                img.Save(path + "/bmp/" + frameList[cnt] + ".bmp");
+                cnt++;
+            }
+            */
+            swk.Close();
+            sww.Close();
+            kinect.Stop();
+            kinect.Dispose();
         }
     }
 }
